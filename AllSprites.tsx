c33e7e541ce9296def55bf3346ff10585d10f742
unknown <francesco.roccabruna@marconirovereto.it>
2024-08-20 19:00:10 +0200

    aggiunto roll

---
+++ AllSprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="AllSprites" tilewidth="128" tileheight="128" margin="64" tilecount="858" columns="39">
+ <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/AllSprites.png" width="5120" height="2880"/>
+</tileset>
---
+++ sd.tsx
@@ -0,0 +1,247 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="sd" tilewidth="1280" tileheight="720" tilecount="81" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Dirt/Dirt1.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Dirt/Dirt1Dark.png" width="320" height="320"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Dirt/DirtObscured.png" width="128" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Bonus/Rock1.png" width="320" height="320"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Bonus/Rock1Dark.png" width="320" height="320"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Bonus/ShadowBonusInside.png" width="384" height="384"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Bonus/ShadowBonusOutside.png" width="384" height="384"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_Bottom.png" width="128" height="128"/>
+ </tile>
+ <tile id="8">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_BottomLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="9">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_BottomRight.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_Left.png" width="128" height="128"/>
+ </tile>
+ <tile id="11">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_Right.png" width="128" height="128"/>
+ </tile>
+ <tile id="12">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_Top.png" width="128" height="128"/>
+ </tile>
+ <tile id="13">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_TopLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="14">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Inside/DirtFade_Inside_TopRight.png" width="128" height="128"/>
+ </tile>
+ <tile id="15">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Bottom.png" width="128" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_BottomLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Bottomright.png" width="128" height="128"/>
+ </tile>
+ <tile id="18">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Left.png" width="128" height="128"/>
+ </tile>
+ <tile id="19">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Right.png" width="128" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Top.png" width="128" height="128"/>
+ </tile>
+ <tile id="21">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Topleft.png" width="128" height="128"/>
+ </tile>
+ <tile id="22">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/DirtFade_Outside/DirtFade_Outside_Topright.png" width="128" height="128"/>
+ </tile>
+ <tile id="23">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_L_Blocks/L_Dirt1.png" width="256" height="256"/>
+ </tile>
+ <tile id="24">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_L_Blocks/L_Dirt2.png" width="256" height="256"/>
+ </tile>
+ <tile id="25">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_L_Blocks/L_Dirt3.png" width="256" height="256"/>
+ </tile>
+ <tile id="26">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_L_Blocks/L_Dirt4.png" width="256" height="256"/>
+ </tile>
+ <tile id="27">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_U_Blocks/Ublocks_Bottom.png" width="256" height="256"/>
+ </tile>
+ <tile id="28">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_U_Blocks/Ublocks_Left.png" width="256" height="256"/>
+ </tile>
+ <tile id="29">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_U_Blocks/Ublocks_Right.png" width="256" height="256"/>
+ </tile>
+ <tile id="30">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Giant_U_Blocks/Ublocks_Top.png" width="256" height="256"/>
+ </tile>
+ <tile id="31">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/A_GrassBottom.png" width="128" height="128"/>
+ </tile>
+ <tile id="32">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/A_GrassTop.png" width="128" height="128"/>
+ </tile>
+ <tile id="33">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/A_GrassVertLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="34">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/A_GrassVertRight.png" width="128" height="128"/>
+ </tile>
+ <tile id="35">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/Grass_FullBlock.png" width="128" height="128"/>
+ </tile>
+ <tile id="36">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassCompleter_BothDown.png" width="128" height="128"/>
+ </tile>
+ <tile id="37">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassCompleter_BothSides.png" width="128" height="128"/>
+ </tile>
+ <tile id="38">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassCompleter_Left.png" width="128" height="128"/>
+ </tile>
+ <tile id="39">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassCompleter_LeftDown.png" width="128" height="128"/>
+ </tile>
+ <tile id="40">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassCompleter_Right.png" width="128" height="128"/>
+ </tile>
+ <tile id="41">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassCompleter_RightDown.png" width="128" height="128"/>
+ </tile>
+ <tile id="42">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassSidesHorizon.png" width="128" height="128"/>
+ </tile>
+ <tile id="43">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass/GrassSidesVert.png" width="128" height="128"/>
+ </tile>
+ <tile id="44">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_Hill/HillsCompleterLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="45">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_Hill/HillsCompleterRight.png" width="128" height="128"/>
+ </tile>
+ <tile id="46">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_L_Block/GrassL_BottomLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="47">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_L_Block/GrassL_BottomRight.png" width="128" height="128"/>
+ </tile>
+ <tile id="48">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_L_Block/GrassL_TopLeft.png" width="128" height="128"/>
+ </tile>
+ <tile id="49">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_L_Block/GrassL_TopRight.png" width="128" height="128"/>
+ </tile>
+ <tile id="50">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_U_Block/Little_UGrass_Bottom.png" width="128" height="128"/>
+ </tile>
+ <tile id="51">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_U_Block/Little_UGrass_Left.png" width="128" height="128"/>
+ </tile>
+ <tile id="52">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_U_Block/Little_UGrass_Right.png" width="128" height="128"/>
+ </tile>
+ <tile id="53">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/WallsAndBlocks/Grass_U_Block/Little_UGrass_Top.png" width="128" height="128"/>
+ </tile>
+ <tile id="54">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/BG.png" width="1280" height="720"/>
+ </tile>
+ <tile id="55">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/Bush1.png" width="68" height="28"/>
+ </tile>
+ <tile id="56">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/Bush2.png" width="64" height="64"/>
+ </tile>
+ <tile id="57">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/Bush3.png" width="112" height="68"/>
+ </tile>
+ <tile id="58">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/CloudBig.png" width="228" height="84"/>
+ </tile>
+ <tile id="59">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/CloudLittle.png" width="112" height="60"/>
+ </tile>
+ <tile id="60">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/DarkerTree_Foot.png" width="64" height="40"/>
+ </tile>
+ <tile id="61">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/DarkerTree_Leaves.png" width="164" height="236"/>
+ </tile>
+ <tile id="62">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/DarkerTree_Log.png" width="40" height="40"/>
+ </tile>
+ <tile id="63">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/DarkTree_Foot.png" width="64" height="40"/>
+ </tile>
+ <tile id="64">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/DarkTree_Leaves.png" width="164" height="236"/>
+ </tile>
+ <tile id="65">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/DarkTree_Log.png" width="40" height="40"/>
+ </tile>
+ <tile id="66">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/FarAwayTree.png" width="116" height="208"/>
+ </tile>
+ <tile id="67">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/FlowerBlue.png" width="44" height="56"/>
+ </tile>
+ <tile id="68">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/FlowerRed.png" width="44" height="56"/>
+ </tile>
+ <tile id="69">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/FlowerYellow.png" width="44" height="56"/>
+ </tile>
+ <tile id="70">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/ForestLake.png" width="1280" height="392"/>
+ </tile>
+ <tile id="71">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/HillDarker.png" width="588" height="260"/>
+ </tile>
+ <tile id="72">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/HillLighter.png" width="436" height="216"/>
+ </tile>
+ <tile id="73">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/Lake.png" width="1008" height="200"/>
+ </tile>
+ <tile id="74">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/LightTree_Foot.png" width="68" height="40"/>
+ </tile>
+ <tile id="75">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/LightTree_Leaves.png" width="164" height="232"/>
+ </tile>
+ <tile id="76">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/LightTree_Log.png" width="40" height="40"/>
+ </tile>
+ <tile id="77">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/mountain.png" width="456" height="220"/>
+ </tile>
+ <tile id="78">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/Mountain_Little.png" width="196" height="112"/>
+ </tile>
+ <tile id="79">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/MushRoom.png" width="36" height="32"/>
+ </tile>
+ <tile id="80">
+  <image source="../../../../CastorC_Forest_PixelPack/CastorC_Forest_PixelPack/Enviroment/Sprite-0007.png" width="1280" height="392"/>
+ </tile>
+</tileset>
